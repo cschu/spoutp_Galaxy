d7cd4d902d2a65e839a1d50970607624c9b2a92e
Chris Schudoma <cschu1981@gmail.com>
2015-03-24 20:04:36 +0000

    Minor.

--- spoutP.py
+++ spoutP.py
@@ -27,8 +27,17 @@ def main(argv):
     
     descr = ''
     parser = argparse.ArgumentParser(description=descr)
-    parser.add_argument('inputFile', type=str, help='The nucleic acid input file.')
-    parser.add_argument('outputFile', type=str, help='The translated coding sequences.')
+    parser.add_argument('--Cmax', default=0.32)
+    parser.add_argument('--Ymax', default=0.33)
+    parser.add_argument('--Smax', default=0.87)
+    parser.add_argument('--Smean', default=0.48)
+    parser.add_argument('--Dmax', default=0.43)
+
+
+    parser.add_argument('spoutFile', type=str, help='.')
+    parser.add_argument('naseqFile', type=str, help='.')
+    parser.add_argument('aaseqFile', type=str, help='.')
+    parser.add_argument('outputFile', type=str, help='.')
     
     try:
         args = parser.parse_args()
@@ -39,7 +48,9 @@ def main(argv):
         sys.stderr.write('Input file (%s) is missing.\n' % args.inputFile)
         sys.exit(1)
 
-    doStuff(args.inputFile, args.outputFile)
+    # doStuff(args.inputFile, args.outputFile)
+    open(args.outputFile, 'wb').write(','.join(map(float, [args.Cmax, args.Ymax, args.Smax, args.Smean, args.Dmax])))
+    
 
 
     pass
