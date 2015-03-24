31266ed30c061e8eb44387807403d762564300ec
Chris Schudoma <cschu1981@gmail.com>
2015-03-24 21:20:35 +0000

    Building...

--- spoutP.py
+++ spoutP.py
@@ -11,11 +11,59 @@ import csv
 
 from biolib import anabl_getContigsFromFASTA as readFASTA, translateCDS
 
+"""
+0: #ID
+1: NN_Cmax_score
+2: NN_Cmax_pos
+3: NN_Cmax_pred
+4: NN_Ymax_score
+5: NN_Ymax_pos
+6: NN_Ymax_pred
+7: NN_Smax_score
+8: NN_Smax_pos
+9: NN_Smax_pred
+10: NN_Smean_score
+11: NN_Smean_pred
+12: NN_D_score
+13: NN_D_pred
+14: HMM_type
+15: HMM_Cmax_score
+16: HMM_Cmax_pos
+17: HMM_Cmax_pred
+18: HMM_Sprob_score
+19: HMM_Sprob_pred
+"""
+PRED_HEADER = ['#SeqID', 'NA_Seq', 'Pep_Seq', 'seqlen_NA', 'seqlen_Pep',
+               'Pos_before_CleavageSite(NN)',
+               'Pos_after_CleavageSite(NN)',
+               'seqlen_Mature',
+               'CleavageSite(NN)',
+               'NA_Seq_w/o_SigP(NN)',
+               'Signal peptide',
+               'Mature peptide']
+
+
 
 def doStuff(args):
+    secreted = []
+    for row in csv.reader(open(args.spoutFile), delimiter='\t', quotechar='"'):
+        # for i, col in enumerate(row): out.write('%i: %s\n' % (i, col))
+        if (float(row[7]) > args.Smax) or (float(row[4]) > args.Ymax) or (float(row[10]) > args.Smean):
+            secreted.append([row[0], None, None, None, None, int(row[5]) - 1, int(row[5]), None, None, None, None, None])
+    pepDict = dict(readFASTA(args.aaseqFile))
+    for secPept in secreted:
+        secPept[2] = pepDict.get(secPept[0], '')
+        secPept[4] = len(secPept[2])
+        secPept[7] = secPept[4] - secPept[5]
+        secPept[8] = ''
+        secPept[10] = secPept[2][:secPept[5]]
+        secPept[11] = secPept[2][secPept[5]:]
+    del pepDict
+
     with open(args.outputFile, 'wb') as out:
-        for row in csv.reader(open(args.spoutFile), delimiter='\t', quotechar='"'):
-            for i, col in enumerate(row): out.write('%i: %s\n' % (i, col))
+        for secPept in secreted:
+            out.write('\n'.join(map(str, secPept)) + '\n')
+        pass
     
     pass
 
